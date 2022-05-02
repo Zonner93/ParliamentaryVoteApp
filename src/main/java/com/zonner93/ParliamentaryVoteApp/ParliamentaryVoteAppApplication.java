c412c12f69bd921c5fd90aeb02fa095b52de80e4
zonner <zonner93@gmail.com>
2022-05-02 21:19:14 +0200

    initial commit

---
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/ParliamentaryVoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.zonner93.ParliamentaryVoteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParliamentaryVoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParliamentaryVoteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zonner93/ParliamentaryVoteApp/ParliamentaryVoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonner93.ParliamentaryVoteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParliamentaryVoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
