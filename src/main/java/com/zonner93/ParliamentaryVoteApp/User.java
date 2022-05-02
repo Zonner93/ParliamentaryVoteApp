2bdfbeb2bc24e7c2d3f1ca2ea321fc5b6cf5ee7b
Adam Pawluczuk <zonner93@gmail.com>
2022-05-02 22:57:43 +0200

    add User class

---
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/User.java
@@ -0,0 +1,19 @@
+package com.zonner93.ParliamentaryVoteApp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String surname;
+    private String email;
+}
