830ed3ce25fcfdd82cdd90e49c0e697db8f952a0
Adam Pawluczuk <zonner93@gmail.com>
2022-05-17 18:15:26 +0200

    add election CRUD

--- src/main/java/com/zonner93/ParliamentaryVoteApp/model/controller/ElectionController.java
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/model/controller/ElectionController.java
@@ -5,15 +5,36 @@ import com.zonner93.ParliamentaryVoteApp.model.service.election.ElectionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/election")
 public class ElectionController {
     private final ElectionService electionService;
 
-    @GetMapping(value = "/election")
+    @PostMapping()
     public void createElection(@RequestBody Election election) {
         electionService.createElection(election);
     }
 
+    @GetMapping(path = "/{id}")
+    public Election getElectionById(@PathVariable long id) {
+        return electionService.getElectionById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Election> getAllElectionList() {
+        return electionService.getAllElectionList();
+    }
+
+    @GetMapping
+    public List<Election> getElectionListByNameContaining(@RequestParam String name) {
+        return electionService.getAllElectionByName(name);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteElectionById(@PathVariable long id) {
+
+    }
 }
--- src/main/java/com/zonner93/ParliamentaryVoteApp/model/service/election/ElectionServiceImpl.java
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/model/service/election/ElectionServiceImpl.java
@@ -5,6 +5,8 @@ import com.zonner93.ParliamentaryVoteApp.model.repository.ElectionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ElectionServiceImpl implements ElectionService {
@@ -13,5 +15,29 @@ public class ElectionServiceImpl implements ElectionService {
     @Override
     public void createElection(Election election) {
         electionRepository.save(election);
+    }
+
+    @Override
+    public List<Election> getAllElectionList() {
+        return electionRepository.findAll();
+    }
+
+    @Override
+    public List<Election> getAllElectionByName(String name) {
+        return electionRepository.findAllByNameContaining(name);
+    }
+
+    @Override
+    public Election getElectionById(long id) {
+        return electionRepository.findById(id);
+    }
+
+    @Override
+    public void deleteElectionById(long id) {
+        if (electionRepository.existsById(id)) {
+            electionRepository.deleteById(id);
+        }
+
+//        TODO: obsługa wyjątku że election o takim id nie istnieje
     }
 }
