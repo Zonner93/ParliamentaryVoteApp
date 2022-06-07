e7dda4e07930d8d224b7940b24db2f25e440b6c8
Michau <michal.marcula@o2.pl>
2022-06-07 14:01:47 +0200

    front v4

--- frontend/wybory/src/components/election.jsx
+++ frontend/wybory/src/components/election.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 
 
-function Election() {
-
-
-
-
+function Election(props) {
 
 
 
     return(
-        <>
-
-        </>
+        <div>
+            <p>ID:{props.id}</p>
+            <p>Nazwa : {props.name}</p>
+            <p>Data rozpoczęcia : {props.startDate}</p>
+            <p>Data zakończenia : {props.endDate}</p>
+            {/* <p>Opis : {props.description}</p> */}
+            <button onClick={function(){
+            }
+            }> Wyświetl szczegóły...</button>
+        </div>
     )
 }
 
 export default Election;
+
+// {
+    //     "id": 1,
+    //     "startDate": "2022-06-07",
+    //     "endDate": "2022-06-25",
+    //     "name": "Wybory parlamentarne 1",
+    //     "description": "jakiś opis wyborów 1",
+    //     "candidateList": []
+    // },
+
--- frontend/wybory/src/components/pages/allElections.jsx
+++ frontend/wybory/src/components/pages/allElections.jsx
@@ -1,22 +1,124 @@
-import React from "react";
+import React, {useState} from "react";
 import Election from "../election.jsx"
 
 
 
 function AllElections(){
 
-
+const[allElections, setAllElections] = useState(all)
 
 
 
 
 
     return(
-        <>
-                <p>wszystkie głosowania</p>
-        </>
+
+            allElections.map(function(singleElection){
+                return <Election
+                    key={singleElection.id}
+                    id={singleElection.id}
+                    startDate={singleElection.startDate}
+                    endDate={singleElection.endDate}
+                    name={singleElection.name}
+                />
+
+            })
     )
 }
 
 
 export default AllElections;
+
+
+
+
+const all = [
+    {
+        "id": 1,
+        "startDate": "2022-06-07",
+        "endDate": "2022-06-25",
+        "name": "Wybory parlamentarne 1",
+        "description": "jakiś opis wyborów 1",
+        "candidateList": []
+    },
+    {
+        "id": 2,
+        "startDate": "2022-06-07",
+        "endDate": "2022-06-25",
+        "name": "Wybory parlamentarne 1",
+        "description": "jakiś opis wyborów 1",
+        "candidateList": []
+    },
+    {
+        "id": 3,
+        "startDate": "2022-06-07",
+        "endDate": "2022-06-25",
+        "name": "Wybory parlamentarne 1",
+        "description": "jakiś opis wyborów 1",
+        "candidateList": []
+    },
+    {
+        "id": 4,
+        "startDate": "2022-06-07",
+        "endDate": "2022-06-25",
+        "name": "Wybory parlamentarne 1",
+        "description": "jakiś opis wyborów 1",
+        "candidateList": []
+    },
+    {
+        "id": 5,
+        "startDate": "2022-06-07",
+        "endDate": "2022-06-25",
+        "name": "Wybory parlamentarne 1",
+        "description": "jakiś opis wyborów 1",
+        "candidateList": []
+    },
+    {
+        "id": 6,
+        "startDate": "qweqwe",
+        "endDate": "qweqwe",
+        "name": "qweqwe",
+        "description": "qweqwewq",
+        "candidateList": []
+    },
+    {
+        "id": 7,
+        "startDate": "qweqwe",
+        "endDate": "qweqwe",
+        "name": "qweqwe",
+        "description": "qweqwewq",
+        "candidateList": []
+    },
+    {
+        "id": 8,
+        "startDate": "qweqweqwewqe",
+        "endDate": "qweqwewqewqe",
+        "name": "qweqwewqewqe",
+        "description": "qweqwewqwqewq",
+        "candidateList": []
+    },
+    {
+        "id": 9,
+        "startDate": "2022-07-06",
+        "endDate": "2022-06-01",
+        "name": "qwe",
+        "description": "qwe",
+        "candidateList": []
+    },
+    {
+        "id": 10,
+        "startDate": "2022-06-19",
+        "endDate": "2022-06-17",
+        "name": "qwe",
+        "description": "qwe",
+        "candidateList": []
+    },
+    {
+        "id": 11,
+        "startDate": "",
+        "endDate": "",
+        "name": "",
+        "description": "",
+        "candidateList": []
+    }
+]
