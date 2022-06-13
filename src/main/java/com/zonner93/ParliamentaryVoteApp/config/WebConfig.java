dabc1cf3fda9dcadc749061b94ec7fcf8becccf5
Michau <michal.marcula@o2.pl>
2022-06-13 18:22:14 +0200

    v12

--- src/main/java/com/zonner93/ParliamentaryVoteApp/config/WebConfig.java
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**");
-        registry.addMapping("/api/elections/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE", "PATCH");
-        registry.addMapping("/api/candidates/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE", "PATCH");
+        registry.addMapping("/api/elections/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+        registry.addMapping("/api/candidates/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
