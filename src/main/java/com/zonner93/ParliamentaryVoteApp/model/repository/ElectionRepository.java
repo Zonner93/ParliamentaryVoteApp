63f0b245fce0d2fc6203e9a6e3eac10c3bded566
Adam Pawluczuk <zonner93@gmail.com>
2022-06-19 10:53:01 +0200

    refactier

--- src/main/java/com/zonner93/ParliamentaryVoteApp/model/repository/ElectionRepository.java
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/model/repository/ElectionRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Repository
 public interface ElectionRepository extends JpaRepository<Election, Long> {
     List<Election> findAllByNameContaining(String name);
+
     Election findById(long id);
+
     List<Election> findByEndDateGreaterThan(LocalDateTime dateCompareTo);
 }
