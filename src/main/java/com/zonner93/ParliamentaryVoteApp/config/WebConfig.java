8622ed53798e524378bbf01786aac4b4bb7c0bba
Adam Pawluczuk <zonner93@gmail.com>
2022-06-19 12:35:37 +0200

    fix cors

--- src/main/java/com/zonner93/ParliamentaryVoteApp/config/WebConfig.java
+++ src/main/java/com/zonner93/ParliamentaryVoteApp/config/WebConfig.java
@@ -1,16 +1,16 @@
-//package com.zonner93.ParliamentaryVoteApp.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE", "PATCH");
-//    }
-//}
+package com.zonner93.ParliamentaryVoteApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE", "PATCH");
+    }
+}
